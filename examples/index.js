2a1e1444889dd98934365a5da2c1d8fa401b3fe4
1024shao <3059680677@qq.com>
2022-10-01 19:29:53 +0800

    feat: 新增 scope 作用域和 walk dfs遍历函数

--- examples/index.js
+++ examples/index.js
@@ -2,8 +2,8 @@ const path = require('path')
 const rollup = require('../lib/rollup')
 
 const entry = path.resolve(__dirname, './main.js')
-
-rollup(entry, 'dist.js')
+debugger
+rollup(entry, path.resolve(__dirname, '../dist/dist.js'))
 
 
 
--- examples/main.js
+++ examples/main.js
@@ -1,2 +1,6 @@
 console.log('hello')
 console.log('world')
+const a = 1
+if (a) {
+  console.log('-------')
+}
